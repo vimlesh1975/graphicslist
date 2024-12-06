349d818af5dbe752f4ec8451b7378b2b2bc3fb30
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2024-12-06 12:24:10 +0530

    click to play audio music

--- main.js
+++ main.js
@@ -19,4 +19,24 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log(index)
   }
   setInterval(moveTrain, 5000);
+
+  // Create the audio element
+const audioElement = document.createElement('audio');
+// Set properties for the audio element
+// audioElement.controls = true; // Display playback controls
+// audioElement.muted = true; // Mute the audio to allow autoplay
+audioElement.autoplay = true; // Automatically start playing
+audioElement.loop = true;     // Repeat the audio indefinitely
+// Set the source URL
+audioElement.src = './aa.mp3'; // Replace with your audio URL
+
+// Append the audio element to the body
+document.body.appendChild(audioElement);
+// Start playing after user interaction
+document.body.addEventListener('click', () => {
+  audioElement.play()
+      .then(() => console.log('Audio is playing'))
+      .catch((error) => console.error('Autoplay failed:', error));
+});
+
 });
